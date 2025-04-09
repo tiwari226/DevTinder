a84cdbcaf77784e86e956bf914145bfde39406a2
Om <tom329636@gmail.com>
2025-04-09 17:35:27 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,51 +1,40 @@
 const express = require("express")
-
+const connectDB = require("./config/database")
 const app = express()
+const User = require("./models/user")
 
-//--------------------------------------------Creating a Server ----------------------------------->>
-
-app.listen(3000, () => {
-  console.log("server successfully and Created on port number 3000")
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    "firstName": "Om",
+    "lastName" : "Tiwari",
+    "emailId" : "omti@gmail.com",
+    "password" : "123@123",
+    "age" : "22",
+    "gender" : "Male"
+  })
+  try {
+    await user.save();
+    res.send("Account Created Succesfully....")
+  } catch (error) {
+    res.status(404).send("Data not saved !!")
+  }
 })
 
 
-// ------------------------------------------------- routing concept----------------------------------------->
-
-// // app.use("/", (req, res) => {
-// //   res.send("Hello srever and is  most important created Om ji..")
-// // })
-
-// app.use("/test/2", (req, res) => {
-//   res.send("Routes the elements at test router and some thing else !!!!!!!!!.............")
-// })
-
-// app.use("/test", (req, res) => {
-//   res.send("Routes the elements at test router")
-// })
-
-//----------------------------------------------multimple route handller-------------------------------->
-
-// app.use("/hello", (req, res, next) => {
-//   console.log("My name is Om tiwari -1 ")
-//     //  res.send("printed hello hello hello hello 1")
-//       next();
-// }, (req, res, next) => {
-//   console.log("My name is Om tiwari-2")
-//     //  res.send("printed hello hello hello hello 2")
-//       next();
-// }, (req, res, next) => {
-//   console.log("My name is Om tiwari - 3")
-//      res.send("printed hello hello hello hello 3")
-//       // next();
-// })
+connectDB().then (()=>{
+  console.log("Database connection Establised....")
+    app.listen(7777, () => {
+    console.log("server successfully and Created on port number 7777")
+  })
+})
+.catch((err) => {
+    console.error("Databases Can't' be connected");
+})
 
 
-//---------------------------------------------------------errorHanding------------------------------------------>
 
-// app.use("/getuser", (err, req, res, next) => {
-//          try {
-//           res.send("hello every thing is right")
-//          } catch (err) {
-//           throw err;
-//          }
-// })
+
+
+
+
+
