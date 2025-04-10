e8bc1df4a0ac962c4449010b060be9783b0a0227
Om <tom329636@gmail.com>
2025-04-10 12:16:30 +0530

    Get API

--- src/app.js
+++ src/app.js
@@ -2,22 +2,51 @@ const express = require("express")
 const connectDB = require("./config/database")
 const app = express()
 const User = require("./models/user")
+app.use(express.json())
 
+
+// API for sign up
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    "firstName": "Om",
-    "lastName" : "Tiwari",
-    "emailId" : "omti@gmail.com",
-    "password" : "123@123",
-    "age" : "22",
-    "gender" : "Male"
-  })
+  const user = new User(req.body)
   try {
     await user.save();
     res.send("Account Created Succesfully....")
   } catch (error) {
     res.status(404).send("Data not saved !!")
   }
+})
+
+
+// get by EmailId
+app.get("/feed", async (req, res) => {
+  const email = req.body.emailId;
+     try { 
+      const user = await User.find({emailId : email});         
+      if(user.length === 0) {
+        req.send("Not any user for this email")
+      }  
+      else{
+        res.send(user);
+      }               
+     } catch (error) {
+      res.status(404).send("User not found")
+     }
+})
+
+// Get all document 
+app.get("/feed", async (req, res) => {
+  const email = req.body.emailId;
+     try { 
+      const user = await User.find({});         
+      if(user.length === 0) {
+        req.send("Not any user")
+      }  
+      else{
+        res.send(user);
+      }               
+     } catch (error) {
+      res.status(404).send("Email not found")
+     }
 })
 
 
